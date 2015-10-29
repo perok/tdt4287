dd35ec203fe205757ac2a5bc6a0f6e0933179ec0
talepre <tale.prestmo@gmail.com>
2015-10-29 11:01:33 +0100

    Add lengthdistribution logic

--- task2.py
+++ task2.py
@@ -28,14 +28,20 @@ for line in f:
     line = line.strip()
     reversed_line = line[::-1]
     #Get longest suffix-prefix match for given string
-    longest_match = st.find_prefixmatch_nr(reversed_line, st.root, 0.25)
+    longest_match = st.find_prefixmatch_nr(reversed_line, st.root, 0.1)
     #Check number of matches
     length_match = len(longest_match)
     if length_match > 0:
         number_of_matches += 1
-        if number_of_matches % 10000 == 0:
-            print number_of_matches
+    length_rest = len(line) - length_match
+    if length_rest in length_distribution:
+        length_distribution[length_rest] += 1
+    else:
+        length_distribution[length_rest] = 1
+
 
 
     
 print "Number of matches: " + str(number_of_matches)
+print "Length distribution"
+print length_distribution
